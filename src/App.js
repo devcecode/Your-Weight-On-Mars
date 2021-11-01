027072bf219fb65b0d8689394cb4b8b1243fdc6d
devcecode <devcecode@gmail.com>
2021-11-01 12:25:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
+import styled from 'styled-components'
+
+const AppStyled = styled.div`
+padding: 1em;
+`
+const Title     = styled.h1`
+text-align: center;
+margin-bottom: 3em;
+`
+const Label     = styled.label`
+font-size: 1em;
+line-height: 1em;
+display: block;
+margin-bottom: 1em;
+`
+const Input     = styled.input`
+font-size: 1em;
+line-height: 1em;
+padding: .5em;
+outline: none;
+border: 1px solid #dddddd;
+border-radius: .2em;
+
+&:focus {
+  border-color: #aaaaaa;
+}
+`
+
+const WeightResult       = styled.div`
+margin-top: .5em;
+`
+const WeightResultParagraph = styled.p``
 function App() {
+
+  const [ weightOnMars, setWeightOnMars ]       = useState(null)
+
+  const calculateWeight = (x, y) => {
+    let mass   = x / 9.807
+    let result = mass * y
+
+    return result
+  }
+
+  const handleInputChange = e => {
+    const currentValue        = e.target.value
+    const weightOnEarth       = parseInt(currentValue)
+    const weightOnMars        = calculateWeight(weightOnEarth, 3.721)
+    setWeightOnMars(weightOnMars)
+
+  }
+
+  console.log(weightOnMars)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppStyled>
+      <Title>Your Weight On Mars</Title>
+      <form>
+        <Label>Enter your weight on earth</Label>
+        <Input type="text" placeholder="Your Weight Here" onChange={handleInputChange}/>
+      </form>
+      <WeightResult>
+        <WeightResultParagraph>Your Weight On Mars is: {weightOnMars}</WeightResultParagraph>
+      </WeightResult>
+
+    </AppStyled>
+  )
 }
 
-export default App;
+export default App
